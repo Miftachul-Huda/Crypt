5491507a2c1db1d1aed3bd73b9a56049213f2fdc
Miftahul Huda <huda_makruf@outlook.com>
2022-09-10 20:33:59 +0700

    Updates

--- demo/web/main.js
+++ demo/web/main.js
@@ -16,10 +16,12 @@ function main() {
 	const decrypt = new Crypt();
 	let count, level, enc_key, dec_key, enc_total, dec_total, data;
 
-	function Generate() {
-		if (_level.value == "") alert("Level Kosong");
-		else _key.value = toHexString(encrypt.Key);
-	}
+	const fromHexString = hexString =>
+		Uint8Array.from(hexString.replace(/\s/g, '').toLowerCase().match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
+	const toHexString = bytes =>
+		bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0').toUpperCase() + ' ', '');
+	const Generate = e =>
+		_level.value !== "" ? _key.value = toHexString(encrypt.Key) : alert("Level Kosong");
 
 	function Run() {
 		if (_text.value == "") alert("Data Kosong");
@@ -31,11 +33,10 @@ function main() {
 			level = parseInt(_level.value);
 			enc_key = fromHexString(_key.value);
 			dec_key = Uint8Array.from(enc_key);
-			Crypt.Solve(dec_key, level);
+			decrypt.Solve(dec_key, level);
 			encrypt.Key = enc_key;
 			decrypt.Key = dec_key;
-			//encrypt.Level = level;
-
+			encrypt.Level = level;
 			enc_total = dec_total = 0;
 			for(let i = 0, time; i < count; i++) {
 				time = performance.now();
@@ -46,18 +47,11 @@ function main() {
 				dec_total += performance.now()-time;
 			}
 			_solve.innerHTML = toHexString(dec_key);
-			_enc.value = `* Bytes = ${data.length}.\n* Times = ${enc_total/count} ms.\n* Bytes Total = ${data.length*count}.\n* Times Total = ${enc_total} ms.\n_________________________________________________\n${toHexString(encrypt.Convert = data)}`;
-			_dec.value = `* Bytes = ${data.length}.\n* Times = ${dec_total/count} ms.\n* Bytes Total = ${data.length*count}.\n* Times Total = ${dec_total} ms.\n_________________________________________________\n${toHexString(decrypt.Convert = data)}`;
+			_enc.value = `* Bytes = ${data.length}.\n* Times = ${enc_total/count} ms.\n* Bytes Total = ${data.length*count}.\n* Times Total = ${enc_total} ms.\n________________________________________\n${toHexString(encrypt.Convert = data)}`;
+			_dec.value = `* Bytes = ${data.length}.\n* Times = ${dec_total/count} ms.\n* Bytes Total = ${data.length*count}.\n* Times Total = ${dec_total} ms.\n________________________________________\n${toHexString(decrypt.Convert = data)}`;
 		}
 	}
 	_gen.addEventListener("click", Generate);
 	_run.addEventListener("click", Run);
 }
-
-const fromHexString = hexString =>
-	Uint8Array.from(hexString.replace(/\s/g, '').toLowerCase().match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
-
-const toHexString = bytes =>
-	bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0').toUpperCase() + ' ', '');
-
 addEventListener('load', main);
